563bce200950797ab4b8a132aa221180b966fc9b
utilrr <gmarkghealy@gmail.com>
2022-06-05 21:23:37 -0400

    Added some button styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,44 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../App.css";
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
 const Navbar = () => {
   return (
     <>
       <h1>MJH</h1>
+
       <ul>
-        <Button variant="contained">
-          <li>
-            <NavLink to="/">About</NavLink>
-          </li>
-        </Button>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-        <li>
-          <NavLink to="/portfolio">Portfolio</NavLink>
-        </li>
-        <li>
-          <NavLink to="/resume">Resume</NavLink>
-        </li>
+        <Stack spacing={5} direction="row">
+          <Button variant="contained" size="large">
+            <li>
+              <NavLink to="/">About</NavLink>
+            </li>
+          </Button>
+          <Button variant="contained" size="large" color="success">
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              color: "yellow",
+              backgroundColor: "orange",
+              borderColor: "green",
+            }}
+          >
+            <li>
+              <NavLink to="/portfolio">Portfolio</NavLink>
+            </li>
+          </Button>
+          <Button variant="contained" size="large">
+            <li>
+              <NavLink to="/resume">Resume</NavLink>
+            </li>
+          </Button>
+        </Stack>
       </ul>
     </>
   );
